28f4ea5282dc8921146389cb292388fe4d641286
Mjiantonio <miantonio@ccc.edu.ph>
2026-03-30 17:08:16 +0800

    Add Analytics API

---
+++ analytics_api/analytics_api.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once '../models/event.php';
+require_once '../models/participant.php';
+require_once '../models/registration.php';
+require_once '../models/database.php';
+
+header("Content-Type: application/json");
+
+
+$database = new Database();
+$db = $database->getConnection();
+
+$event = new Event($db);
+$participant = new Participant($db);
+$reg = new Registration($db);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+
+$input = json_decode(file_get_contents("php://input"), true);
+
+if ($method === 'GET') {
+    if (isset($_GET['participants_per_event'])) {
+     $participant_per_event = $event->getParticipantsCountPerEvent();
+        if ($participant_per_event && count($participant_per_event) > 0) {
+        echo json_encode($participant_per_event);
+        } else {
+        echo json_encode(["message" => "No Events Listed."]);
+        }   
+    }
+    elseif (isset($_GET['most_popular'])) {
+     $most_popular = $event->getMostPopularEvent();
+        if ($most_popular && count($most_popular) > 0) {
+        echo json_encode($most_popular);
+        } else {
+        echo json_encode(["message" => "No Events Listed."]);
+        }   
+    }
+    elseif (isset($_GET['total_reg'])) {
+     $total_reg = $reg->getTotalRegistrations();
+        if ($total_reg && count($total_reg) > 0) {
+        echo json_encode($total_reg);
+        } else {
+        echo json_encode(["message" => "No Events Listed."]);
+        }   
+    }
+}
